d4f09a23686a869b019952c94cc95dadc2ed8ca3
JPMarichal <jpmarichal@gmail.com>
2024-02-13 07:07:10 -0600

    Estructura base para el uso de SASS

--- resources/views/components/main_header.blade.php
+++ resources/views/components/main_header.blade.php
@@ -1,9 +1,20 @@
-<div class="main-header">
-    @include('components.logo')
-    
-    <div class="menu">
-        @include('components.main_menu')
+<div class="main-header bg-white">
+    <div class="container">
+        <div class="row align-items-center">
+            <!-- Logo -->
+            <div class="col-2">
+                @include('components.logo')
+            </div>
+            
+            <!-- Main Menu -->
+            <div class="col-8">
+                @include('components.main_menu')
+            </div>
+            
+            <!-- Avatar Menu -->
+            <div class="col-2 text-end">
+                @include('components.avatar_menu')
+            </div>
+        </div>
     </div>
-    
-    @include('components.avatar_menu')
 </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,6 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome -->
     <link href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" rel="stylesheet">
+    <!-- Fuentes principales -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- CSS principal -->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
