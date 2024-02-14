bee4c3f6bd5bc287b9910f972839415603e037a0
JPMarichal <jpmarichal@gmail.com>
2024-02-14 16:46:16 -0600

    Se agrega estilo para la imagen destacada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @include('layouts.header')
 
-<h2 class="mt-5">Este es el Home</h2>
+<h1>Este es el Home</h1>
+<img src="https://live.staticflickr.com/8464/8111811112_473a9c9f16_b.jpg" class="featured_image" alt="Imagen de la casa">
+
 
 @include('layouts.footer')
