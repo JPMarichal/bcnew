ea720504a57133134a0d5f57adeaec7b8e60893b
JPMarichal <jpmarichal@gmail.com>
2024-02-13 20:16:46 -0600

    Laravel DebugBar instalado

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,6 +12,29 @@
     <link href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" rel="stylesheet">
     <!-- Vite: tus estilos y JS personalizados -->
     @vite(['resources/sass/styles.scss', 'resources/js/app.js'])
+
+    <!-- Facebook login -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 </head>
 <body>
 <div class="container-fluid my-0 mx-0";>
