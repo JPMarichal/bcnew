4c27927664a7adf83b267f7ca96d54698f04d3b3
JPMarichal <jpmarichal@gmail.com>
2024-02-14 07:46:26 -0600

    Se usa flex para asegurar un header responsible

--- resources/views/components/main_header.blade.php
+++ resources/views/components/main_header.blade.php
@@ -1,20 +1,16 @@
-<div class="main-header bg-white border">
-    <div class="container-fluid">
-        <div class="row align-items-center">
-            <!-- Logo -->
-            <div class="col-2">
-                @include('components.logo')
-            </div>
-            
-            <!-- Main Menu -->
-            <div class="col-8">
-                @include('components.main_menu')
-            </div>
-            
-            <!-- Avatar Menu -->
-            <div class="col-2 text-end">
-                @include('components.avatar_menu')
-            </div>
-        </div>
+<div class="main-header bg-white border" style="display: flex; justify-content: space-between; align-items: center;">
+    <div class="logo">
+        <!-- Logo, ajusta para ocupar espacio adecuado en móviles y más en pantallas grandes -->
+        @include('components.logo')
+    </div>
+
+    <!-- Main Menu, se ajusta para usar el espacio disponible de manera efectiva -->
+    <div class="main-menu flex-grow-1" style="display: flex; flex-grow: 1;">
+        @include('components.main_menu')
+    </div>
+
+    <!-- Avatar Menu, ajusta para ocupar espacio restante en móviles -->
+    <div class="avatar-menu" style="align-self: center;">
+        @include('components.avatar_menu')
     </div>
 </div>
--- resources/views/components/main_menu.blade.php
+++ resources/views/components/main_menu.blade.php
@@ -1,7 +1,14 @@
-<nav class="main-menu navbar navbar-light mt-0 mx-auto">
-    <ul class="nav">
+<nav class="navbar navbar-expand-sm navbar-light mt-0 mx-auto">
+  <div class="container-fluid">
+    <!-- Botón hamburguesa para pantallas pequeñas -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Contenido del menú -->
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" href="/">Inicio</a>
+          <a class="nav-link" href="/">Inicio</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -15,10 +22,12 @@
           </ul>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
+          <a class="nav-link" href="#">Contacto</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Acerca de</a>
+          <a class="nav-link" href="#">Acerca de</a>
         </li>
-    </ul>
+      </ul>
+    </div>
+  </div>
 </nav>
