9c0b688bb9a5ae900f30521c554f35b688d41662
JPMarichal <jpmarichal@gmail.com>
2024-02-14 22:39:23 -0600

    Estilos adicionales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,23 @@
 
 <h1>Este es el Home</h1>
 <img src="https://live.staticflickr.com/8464/8111811112_473a9c9f16_b.jpg" class="featured_image" alt="Imagen de la casa">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, eum. Aperiam nobis harum sapiente, iusto odio vel fugit praesentium autem doloremque corporis ducimus, voluptas quasi totam quia ipsum delectus ipsam.</p>
+<h2>Speed for race</h2>
+<p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi accusamus id</span></p>
+<ul>
+    <li>First things first</li>
+    <li>Second things second</li>
+    <li>Any thing must hurry</li>
+</ul>
+<p> aliquid veniam quaerat, iste est porro quidem architecto consectetur ipsum obcaecati totam, eius corrupti laudantium exercitationem nisi suscipit.</span></p>
+
+<blockquote>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quasi repellendus aspernatur praesentium illo hic vero eum asperiores, id aliquid optio ipsum. Voluptatum adipisci rem cumque reprehenderit ad nesciunt eveniet.</p>
+    <cite>Some sources are hidden</cite>
+</blockquote>
+<p><span>Odio iusto autem sit ullam quidem, provident sequi? Ea necessitatibus ipsa ducimus similique, est nisi omnis cupiditate repellat vitae perferendis? Illum ipsam voluptates similique ullam in iste accusantium. Voluptatum, eum?</span></p>
+<h3>Anything goes faster</h3>
+<p><span>Ab a magnam reprehenderit exercitationem aperiam adipisci culpa labore dignissimos nihil voluptates at dolore debitis dolor impedit perspiciatis necessitatibus veniam ut, porro voluptatum dolores similique quisquam? Itaque tempora eaque officiis.</span><span>Doloribus repudiandae voluptatibus iusto dolore cumque ex obcaecati deleniti ab, aperiam quis inventore dolorem non accusantium, deserunt fugit ipsa. Ducimus labore aspernatur minima enim ipsum veniam rerum quas, atque exercitationem?</span></p>
 
 
 @include('layouts.footer')
