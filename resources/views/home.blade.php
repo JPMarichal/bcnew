19180ce7dd2ed842339100e13f9feadaad9d29f7
JPMarichal <jpmarichal@gmail.com>
2024-02-15 07:49:57 -0600

    Opciones adicionales de tipografía

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,15 +10,25 @@
     <li>Second things second</li>
     <li>Any thing must hurry</li>
 </ul>
-<p> aliquid veniam quaerat, iste est porro quidem architecto consectetur ipsum obcaecati totam, eius corrupti laudantium exercitationem nisi suscipit.</span></p>
+<p> aliquid veniam quaerat, iste est porro quidem architecto consectetur ipsum obcaecati totam, <a href="#" title="Whatever">eius corrupti laudantium</a> exercitationem nisi suscipit.</span></p>
 
 <blockquote>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quasi repellendus aspernatur praesentium illo hic vero eum asperiores, id aliquid optio ipsum. Voluptatum adipisci rem cumque reprehenderit ad nesciunt eveniet.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quasi repellendus aspernatur <i>praesentium illo hic</i> vero eum asperiores, <b>id aliquid optio ipsum</b>. Voluptatum adipisci rem cumque reprehenderit ad nesciunt eveniet.</p>
     <cite>Some sources are hidden</cite>
 </blockquote>
 <p><span>Odio iusto autem sit ullam quidem, provident sequi? Ea necessitatibus ipsa ducimus similique, est nisi omnis cupiditate repellat vitae perferendis? Illum ipsam voluptates similique ullam in iste accusantium. Voluptatum, eum?</span></p>
 <h3>Anything goes faster</h3>
+<ol>
+    <li>First things first</li>
+    <li>Second things second</li>
+    <li>Any thing must hurry</li>
+</ol>
 <p><span>Ab a magnam reprehenderit exercitationem aperiam adipisci culpa labore dignissimos nihil voluptates at dolore debitis dolor impedit perspiciatis necessitatibus veniam ut, porro voluptatum dolores similique quisquam? Itaque tempora eaque officiis.</span><span>Doloribus repudiandae voluptatibus iusto dolore cumque ex obcaecati deleniti ab, aperiam quis inventore dolorem non accusantium, deserunt fugit ipsa. Ducimus labore aspernatur minima enim ipsum veniam rerum quas, atque exercitationem?</span></p>
-
+<h4>Something</h4>
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde aspernatur suscipit inventore molestias enim quasi vero illum, nemo maxime dolore rerum magnam est quia, fugit minima impedit, praesentium laudantium placeat.
+<h5>Anything</h5>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores accusamus ipsum non obcaecati, velit possimus autem laboriosam pariatur illum et earum temporibus qui! Assumenda magnam, quis ullam maiores quaerat consectetur?
+<h6>Anything</h6>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam voluptate corporis ea sequi accusantium. Pariatur, aliquid? Minus maiores eos, quaerat nihil excepturi corporis animi ut? Aperiam, at? Sapiente, assumenda natus?
 
 @include('layouts.footer')
