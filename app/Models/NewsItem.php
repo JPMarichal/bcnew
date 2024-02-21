d130f452b140321e6e1efdfed3e9ffaca3efddb7
JPMarichal <jpmarichal@gmail.com>
2024-02-20 21:29:47 -0600

    Creación del modelo NewsItem

---
+++ app/Models/NewsItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewsItem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'news_items';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'link',
+        'pub_date',
+        'source',
+        'featured_image',
+        'content',
+    ];
+
+    protected $casts = [
+        'pub_date' => 'datetime',
+    ];
+}
