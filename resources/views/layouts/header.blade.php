acfca57fe910b7a11c9b5784bfda316163b7a564
JPMarichal <jpmarichal@gmail.com>
2024-03-04 06:59:31 -0600

    Se agrega favicon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autenticación con Redes Sociales</title>
+    <link rel="icon" href="{{ asset('icons/favicon.png') }}" type="image/png">
     <!-- Fuentes principales -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.bunny.net">
