b0e0f5d50f9cd80abd071b80bafb68464ea54966
JPMarichal <jpmarichal@gmail.com>
2024-03-04 22:25:18 -0600

    Se prefijan todas las clases de tailwind para evitar conflictos con bootstrap

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    prefix: 'tw-', // Añadir esta línea para prefijar todas las clases de Tailwind con "tw-"
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
