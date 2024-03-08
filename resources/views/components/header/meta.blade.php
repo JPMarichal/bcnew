858eb7aa94ac28b202ac9b1be492b54a69ab999e
JPMarichal <jpmarichal@gmail.com>
2024-03-08 16:05:48 -0600

    Se agrega el meta keywords

--- resources/views/components/header/meta.blade.php
+++ resources/views/components/header/meta.blade.php
@@ -5,6 +5,7 @@
 <!-- Título y descripción general -->
 <title>@yield('title', 'Biblicomentarios.com')</title>
 <meta name="description" content="@yield('description', 'Análisis de la Biblia y de las revelaciones modernas')">
+<meta name="keywords" content="@yield('keywords', 'biblia,santa biblia,libro de mormón,ven sígueme,lds,sud,mormones,fe,dios, revelaciones, análisis, religión, espiritualidad,escrituras')">
 <meta name="language" content="Spanish">
 
 <link rel="manifest" href="manifest.webmanifest">
