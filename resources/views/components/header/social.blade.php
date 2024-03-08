c3a63d7be119af661da48f2b4f0499aad1d9256e
JPMarichal <jpmarichal@gmail.com>
2024-03-08 07:24:35 -0600

    Se crean etiquetas especiales para productos

--- resources/views/components/header/social.blade.php
+++ resources/views/components/header/social.blade.php
@@ -19,8 +19,11 @@
 <meta property="og:image:width" content="960">
 <meta property="og:image:height" content="540">
 <meta property="og:image:type" content="image/jpeg">
+<meta property="og:image:alt" content="@yield('title', 'Biblicomentarios.com')">
 
 <meta property="fb:admins" content="@yield('fb_admins', '590703081059319')">
+<meta property="fb:app_id" content="@yield('fb_admins', '590703081059319')">
+
 {{-- Twitter --}}
 <!-- Tags específicos de Twitter -->
 <meta name="twitter:card" content="summary_large_image">
@@ -34,14 +37,16 @@
 <meta name="twitter:data1" content="@yield('twitter_author', 'Juan Pablo Marichal')">
 
 {{-- Etiquetas específicas para producto --}}
-<meta property="og:price:amount" content="15.00" />
-<meta property="og:price:currency" content="USD" />
-<meta name="twitter:data1" content="$3">
-<meta name="twitter:label1" content="Price">
-<meta name="twitter:data2" content="Black">
-<meta name="twitter:label2" content="Color">
+@if(trim($__env->yieldContent('type')) === 'product')
+    <meta property="og:price:amount" content="15.00" />
+    <meta property="og:price:currency" content="USD" />
+    <meta name="twitter:data1" content="$3">
+    <meta name="twitter:label1" content="Price">
+    <meta name="twitter:data2" content="Black">
+    <meta name="twitter:label2" content="Color">
+@endif
 
+@endif
 {{-- URL Canónica --}}
 <!-- URL Canónica -->
 <link rel="canonical" href="@yield('canonical_url', request()->url())">
-
