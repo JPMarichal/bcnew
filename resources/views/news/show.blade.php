435e007b78b17b222669e11853be18540eb31c0a
JPMarichal <jpmarichal@gmail.com>
2024-03-09 04:42:27 -0600

    Se agregan etiquetas semánticas

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -12,30 +12,33 @@
 
 @section('schema_markup')
 <script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "NewsArticle",
-  "mainEntityOfPage": {
-    "@type": "WebPage",
-    "@id": "{{ url()->current() }}"
-  },
-  "headline": "{{ $newsItem->title }}",
-  "image": [
-    "{{ $newsItem->featured_image }}"
-  ],
-  "datePublished": "{{ \Carbon\Carbon::parse($newsItem->pub_date)->toIso8601String() }}",
-  "dateModified": "{{ \Carbon\Carbon::parse($newsItem->updated_at)->toIso8601String() }}",
-  "author": {
-    "@type": "Person",
-    "name": "Juan Pablo Marichal"
-  },
-  "publisher": {
-    "@type": "Organization",
-    "name": "BcNew"
-    // Aquí se omitió el "logo"
-  },
-  "description": "{{ $newsItem->description }}"
-}
+    {
+        "@context": "https://schema.org",
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "{{ url()->current() }}"
+        },
+        "headline": "{{ $newsItem->title }}",
+        "image": [
+            "{{ $newsItem->featured_image }}"
+        ],
+        "datePublished": "{{ \Carbon\Carbon::parse($newsItem->pub_date)->toIso8601String() }}",
+        "dateModified": "{{ \Carbon\Carbon::parse($newsItem->updated_at)->toIso8601String() }}",
+        "author": {
+            "@type": "Person",
+            "name": "Juan Pablo Marichal"
+        },
+        "publisher": {
+            "@type": "Organization",
+            "name": "Biblicomentarios",
+            "logo": {
+                "@type": "ImageObject",
+                "url": "{{ asset('android-chrome-512x512.png') }}"
+            }
+        },
+        "description": "{{ $newsItem->description }}"
+    }
 </script>
 @endsection
 
@@ -59,24 +62,26 @@
 </style>
 
 <div class="container mt-5">
-    <h1>{{ $newsItem->title }}</h1>
+    <article>
+        <h1>{{ $newsItem->title }}</h1>
 
-    <x-news.meta-bar :newsItem="$newsItem" />
+        <x-news.meta-bar :newsItem="$newsItem" />
 
-    <div class="row">
-        <div class="col-md-1">
-            <x-social-share-bar />
+        <div class="row">
+            <div class="col-md-1">
+                <x-social-share-bar />
+            </div>
+
+            <div class="col-md-11">
+                <img src="{{ $newsItem->featured_image }}" class="news-image" alt="{{ $newsItem->title }}" title="{{ $newsItem->title }}">
+                {!! $newsItem->content !!}
+                @livewire('news-suggestion')
+            </div>
         </div>
 
-        <div class="col-md-11">
-            <img src="{{ $newsItem->featured_image }}" class="news-image" alt="{{ $newsItem->title }}" title="{{ $newsItem->title }}">
-            {!! $newsItem->content !!}
-            @livewire('news-suggestion')
-        </div>
-    </div>
-
-    <a href="{{ route('noticias.index') }}" class="btn btn-primary mt-3">
-        <i class="fas fa-arrow-left"></i> Regresar al listado
-    </a>
+        <a href="{{ route('noticias.index') }}" class="btn btn-primary mt-3">
+            <i class="fas fa-arrow-left"></i> Regresar al listado
+        </a>
+    </article>
 </div>
 @endsection
