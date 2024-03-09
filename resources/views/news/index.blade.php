ecc5db1354f95915c30f7025e56bc8cb674f2c5a
JPMarichal <jpmarichal@gmail.com>
2024-03-09 04:29:29 -0600

    Se aplican etiquetas semánticas

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -13,21 +13,20 @@
     @section('twitter_author', 'JPMarichal')
 @endif
 
-
 @section('content')
 <div class="container mt-5">
     <h1>Noticias de la Iglesia de Jesucristo <br />de los Santos de los Ultimos Días</h1>
     <x-news.filters :years="$years" />
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach ($news as $newsItem)
-        <div class="col">
+        <article class="col">
             <div class="card h-100">
                 <a href="{{ route('noticias.show', $newsItem->slug) }}">
                     <img src="{{ $newsItem->featured_image }}" class="card-img-top" alt="{{ $newsItem->title }}">
                 </a>
                 <div class="card-body">
                     <a href="{{ route('noticias.show', $newsItem->slug) }}" style="text-decoration: none; color: inherit;">
-                        <h3 class="card-title">{{ $newsItem->title }}</h3>
+                        <h2 class="card-title">{{ $newsItem->title }}</h2>
                     </a>
                     <p class="card-text text-muted" style="font-size: 0.8rem;">
                         {{ \Carbon\Carbon::parse($newsItem->pub_date)->format('d M, Y') }} - {{ $newsItem->country }}
@@ -37,7 +36,7 @@
                     <a href="{{ route('noticias.show', $newsItem->slug) }}" class="btn btn-primary">Leer más</a>
                 </div>
             </div>
-        </div>
+        </article>
         @endforeach
     </div>
     <!-- Paginación -->
