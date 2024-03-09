0be1b5eb7d4b4e572df600fac7252a5e52d9e8eb
JPMarichal <jpmarichal@gmail.com>
2024-03-09 04:34:25 -0600

    Se agregan etiquetas semánticas

--- resources/views/components/custom-pagination.blade.php
+++ resources/views/components/custom-pagination.blade.php
@@ -1,36 +1,38 @@
 @if ($paginator->hasPages())
-    <div class="pagination-container">
-        {{-- Enlace de página anterior --}}
-        @if ($paginator->onFirstPage())
-            <span class="page-link disabled">Anterior</span>
-        @else
-            <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">Anterior</a>
-        @endif
-
-        {{-- Elementos de paginación --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <span class="page-link disabled">{{ $element }}</span>
+    <nav aria-label="Navegación de página">
+        <div class="pagination-container">
+            {{-- Enlace de página anterior --}}
+            @if ($paginator->onFirstPage())
+                <span class="page-link disabled" aria-disabled="true">Anterior</span>
+            @else
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">Anterior</a>
             @endif
 
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <span class="page-link active">{{ $page }}</span>
-                    @else
-                        <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+            {{-- Elementos de paginación --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <span class="page-link disabled" aria-disabled="true">{{ $element }}</span>
+                @endif
 
-        {{-- Enlace de página siguiente --}}
-        @if ($paginator->hasMorePages())
-            <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Siguiente</a>
-        @else
-            <span class="page-link disabled">Siguiente</span>
-        @endif
-    </div>
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <span class="page-link active" aria-current="page">{{ $page }}</span>
+                        @else
+                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Enlace de página siguiente --}}
+            @if ($paginator->hasMorePages())
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Siguiente</a>
+            @else
+                <span class="page-link disabled" aria-disabled="true">Siguiente</span>
+            @endif
+        </div>
+    </nav>
 @endif
