326f006068334cf8fc3303c0f512273bbab9dd05
JPMarichal <jpmarichal@gmail.com>
2024-03-09 09:27:24 -0600

    Corrección a opciones de navegación

--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -6,6 +6,13 @@
 <main class="container text-center mt-5">
     <h1>Error en la Aplicación</h1>
     <p>Lo sentimos, ha ocurrido un error inesperado.</p>
-    <a href="{{ url('/') }}" class="btn btn-primary">Volver al inicio</a>
+    <nav aria-label="Opciones de navegación tras el error de tiempo de espera">
+        <ul class="list-unstyled">
+            <li><a href="/" class="text-primary">Inicio</a></li>
+            <li><a href="/noticias" class="text-primary">Últimas Noticias</a></li>
+            <li><a href="/contacto" class="text-primary">Contacto</a></li>
+        </ul>
+    </nav>
+    <a href="/" class="btn btn-primary">Volver al inicio</a>
 </main>
 @endsection
