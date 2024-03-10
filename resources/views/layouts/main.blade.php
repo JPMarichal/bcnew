84d912f326ac2237bc79b1650110302e3d8e39b9
JPMarichal <jpmarichal@gmail.com>
2024-03-09 22:12:37 -0600

    Se implementa paquete de gestión de cookies

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" prefix="og: https://ogp.me/ns#">
+
 <head>
     @include('layouts.header')
+    @cookieConsent
 </head>
+
 <body>
+    @cookieConsent
     <header>
         @include('components.main_header')
     </header>
@@ -16,4 +20,5 @@
         @include('layouts.footer')
     </footer>
 </body>
+
 </html>
