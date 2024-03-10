5cedd31cd114493461082191abdb2efb4a2631b7
JPMarichal <jpmarichal@gmail.com>
2024-03-10 06:49:48 -0600

    Configuraciones iniciales de tinyMCE

--- public/js/tinymce-setup.js
+++ public/js/tinymce-setup.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     tinymce.init({
-        selector: 'textarea',  
+        selector: 'textarea',
         // Configuraciones globales básicas
-        menubar: true, 
+        menubar: true,
         plugins: [
             'advlist autolink lists link image charmap print preview anchor',
             'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
+            'insertdatetime media table paste code help wordcount',
+            'table', // Añade el plugin de tabla aquí
+            'blockquote', // No es necesario añadirlo como plugin, pero sí asegurarte de que el botón esté en la toolbar si deseas un botón específico para blockquote
         ],
         toolbar: 'undo redo | formatselect | ' +
-        'bold italic backcolor | alignleft aligncenter ' +
+        'bold italic | alignleft aligncenter ' +
         'alignright alignjustify | bullist numlist outdent indent | ' +
-        'removeformat | help'
+        'removeformat | help | ' +
+        'table | blockquote | ' + // Añade los botones de tabla y blockquote aquí
+        'h1 h2 h3', // Añade botones para insertar encabezados
     });
 });
