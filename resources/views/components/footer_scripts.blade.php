bf38f7856602e9a812ef503f36d442d8ad9c0ccf
JPMarichal <jpmarichal@gmail.com>
2024-03-11 04:31:55 -0600

    Se separan los scripts de pie de página en el componente footer_scripts

---
+++ resources/views/components/footer_scripts.blade.php
@@ -0,0 +1,9 @@
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+<!-- Popper.js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- TinyMCE -->
+<script src="{{ asset('js/tinymce-setup.js') }}"></script>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,7 @@
 <div class="border p-2">Este es el footer.</div>
 </div> <!-- Cierre del container -->
 
-<!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
-<!-- Popper.js -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- TinyMCE -->
-<script src="{{ asset('js/tinymce-setup.js') }}"></script>
+@include('components.footer_scripts')
 
 </body>
 </html>
