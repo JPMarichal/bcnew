b5d34402dffc610044efb846d0afe49315c483f1
JPMarichal <jpmarichal@gmail.com>
2024-03-11 04:25:57 -0600

    Configuración de funcionalidad y apariencia de tinyMCE

--- public/js/tinymce-setup.js
+++ public/js/tinymce-setup.js
@@ -2,21 +2,49 @@ document.addEventListener('DOMContentLoaded', function () {
     tinymce.init({
         selector: 'textarea',
         menubar: true,
+        language: 'es',
         plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
-            'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table',  'code', 'help', 'wordcount'
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'code',
+            'fullscreen',
+            'media',
+            'table',
+            'help',
+            'wordcount',
+            'emoticons',
+            'autosave',
+            'autoresize',
+            'quickbars'
         ],
-        toolbar: 'undo redo | formatselect | ' +
-        'bold italic | alignleft aligncenter ' +
-        'alignright alignjustify | bullist numlist outdent indent | ' +
-        'removeformat | help | ' +
-        'table | blockquote | ' +
-        'h1 h2'
+        toolbar: 
+            ['undo redo | formatselect | ' +
+            'bold italic | ' +
+            'alignleft aligncenter alignright alignjustify | ' +
+            'bullist numlist | ' ,
+            'link unlink image media table| ' +
+            'emoticons | ' +
+            'removeformat | '+
+            'preview fullscreen | help ' +
+            ' code'],
+        quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+        autosave_ask_before_unload: true,
+        autosave_interval: "30s",
+        autosave_retention: "2m",
+        autoresize_bottom_margin: 50,
+        end_container_on_empty_block: true,
+        height: 200,
     });
 
-    document.querySelectorAll('form').forEach(function(form) {
-        form.addEventListener('submit', function() {
+    document.querySelectorAll('form').forEach(function (form) {
+        form.addEventListener('submit', function () {
             tinymce.triggerSave();
         });
     });
