25d0659057c9142c64bad818e69f27d912055725
JPMarichal <jpmarichal@gmail.com>
2024-03-11 18:28:00 -0600

    Se resalta la opción activa en el menú principal

--- resources/views/components/main_menu.blade.php
+++ resources/views/components/main_menu.blade.php
@@ -8,25 +8,25 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/">Inicio</a>
+          <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="/">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/noticias">Noticias de la Iglesia</a>
+          <a class="nav-link {{ request()->is('noticias') || request()->is('noticias/*') ? 'active' : '' }}" href="/noticias">Noticias de la Iglesia</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Artículos</a>
+          <a class="nav-link {{ request()->is('articulos') || request()->is('articulos/*') ? 'active' : '' }}" href="#">Artículos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Estudio de las Escrituras</a>
+          <a class="nav-link {{ request()->is('estudio-de-las-escrituras') || request()->is('estudio-de-las-escrituras/*') ? 'active' : '' }}" href="#">Estudio de las Escrituras</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Glosarios</a>
+          <a class="nav-link {{ request()->is('glosarios') || request()->is('glosarios/*') ? 'active' : '' }}" href="#">Glosarios</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{route('site.contact')}}">Contacto</a>
+          <a class="nav-link {{ request()->routeIs('site.contact') ? 'active' : '' }}" href="{{route('site.contact')}}">Contacto</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{route('site.about')}}">Acerca de</a>
+          <a class="nav-link {{ request()->routeIs('site.about') ? 'active' : '' }}" href="{{route('site.about')}}">Acerca de</a>
         </li>
       </ul>
     </div>
