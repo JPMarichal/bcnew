94d96f309213d0f9a2e586bd8ab1e5ad2a5abf5f
JPMarichal <jpmarichal@gmail.com>
2024-04-01 06:58:29 -0600

    Se divide el menú de backpack en combos

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,8 +1,15 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+        {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-item title="Users" icon="la la-users" :link="backpack_url('user')" />
-<x-backpack::menu-item title="Newsitems" icon="la la-newspaper" :link="backpack_url('newsitem')" />
-<x-backpack::menu-item title="Newsposts" icon="la la-newspaper" :link="backpack_url('newspost')" />
-<x-backpack::menu-item title="Volúmenes" icon="la la-book" :link="backpack_url('escrituras/volumen')" />
-<x-backpack::menu-item title="Partes" icon="la la-book" :link="backpack_url('escrituras/parte')" />
+
+<x-backpack::menu-dropdown title="Escrituras" icon="la la-book">
+    <x-backpack::menu-dropdown-item title="Volúmenes" icon="la la-book" :link="backpack_url('escrituras/volumen')" />
+    <x-backpack::menu-dropdown-item title="Partes" icon="la la-book" :link="backpack_url('escrituras/parte')" />
+</x-backpack::menu-dropdown>
+
+<x-backpack::menu-dropdown title="Noticias" icon="la la-newspaper">
+    <x-backpack::menu-dropdown-item title="Newsitems" icon="la la-newspaper" :link="backpack_url('newsitem')" />
+    <x-backpack::menu-dropdown-item title="Newsposts" icon="la la-newspaper" :link="backpack_url('newspost')" />
+</x-backpack::menu-dropdown>
