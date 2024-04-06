b3e2df254c6a1e4e9aa4f0ceedabddb041d75de4
JPMarichal <jpmarichal@gmail.com>
2024-04-06 10:06:29 -0600

    Mejoras a la presentación del index del blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container mt-5">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-        @foreach($posts as $post)
-            <div class="col">
-                <div class="card h-100">
-                    <div class="m-2 p-2 border rounded" style="height:100px;">
-                    <!-- Asume que $post->image_url es la URL de la imagen del post -->
-                    <a href="{{ route('blog.show', $post->slug) }}">
-                        <img src="{{ $post->featuredImageUrl() }}" class="card-img-top" alt="{{ $post->title }}">
-                    </a>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <a href="{{ route('blog.show', $post->slug) }}">{{ $post->title }}</a>
-                        </h5>
-                        <p class="card-text text-small">{{ $post->excerpt }}</p>
-                    </div>
-                    <div class="card-footer text-end">
-                        <a href="{{ route('blog.show', $post->slug) }}" class="btn btn-primary btn-sm">
-                            <i class="fas fa-arrow-circle-right"></i> Leer ahora
-                        </a>
+    <div class="container mt-5">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            @foreach ($posts as $post)
+                <div class="col">
+                    <div class="card h-100">
+                        <div class="m-2 p-2 border rounded" style="height:100px;">
+                            <!-- Asume que $post->image_url es la URL de la imagen del post -->
+                            <a href="{{ route('blog.show', $post->slug) }}">
+                                <img src="{{ $post->featuredImageUrl() }}" class="card-img-top" style="height: 100px; object-fit: cover;" alt="{{ $post->title }}">
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a class="text-success" href="{{ route('blog.show', $post->slug) }}">{{$post->id}} {{ $post->title }}</a>
+                            </h5>
+                            <p class="card-text small text-muted">{{ $post->excerpt }}</p>
+                        </div>
+                        <div class="card-footer text-end">
+                            <a href="{{ route('blog.show', $post->slug) }}" class="btn btn-primary btn-sm">
+                                <i class="fas fa-arrow-circle-right"></i> Leer ahora
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
+        <div class="mt-4">
+            {{ $posts->links() }}
+        </div>
     </div>
-    <div class="mt-4">
-        {{ $posts->links() }}
-    </div>
-</div>
 @endsection
