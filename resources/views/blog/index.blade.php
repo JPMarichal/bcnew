bd5273132cc3dbc69fda9de37ec4b29e4af27d2a
JPMarichal <jpmarichal@gmail.com>
2024-04-07 23:48:16 -0600

    Modificaciones de formato a index y show de blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,19 +9,34 @@
                         <div class="m-2 p-2 border rounded" style="height:100px;">
                             <!-- Asume que $post->image_url es la URL de la imagen del post -->
                             <a href="{{ route('blog.show', $post->slug) }}">
-                                <img src="{{ $post->featuredImageUrl() }}" class="card-img-top" style="height: 100px; object-fit: cover;" alt="{{ $post->title }}">
+                                <img src="{{ $post->featuredImageUrl() }}" class="card-img-top"
+                                    style="height: 100px; object-fit: cover;" alt="{{ $post->title }}">
                             </a>
                         </div>
                         <div class="card-body">
                             <h5 class="card-title">
-                                <a class="text-success" href="{{ route('blog.show', $post->slug) }}">{{$post->id}} {{ $post->title }}</a>
+                                <a class="text-success" href="{{ route('blog.show', $post->slug) }}">
+                                    {{ $post->title }}
+                                </a>
                             </h5>
-                            <p class="card-text small text-muted">{{ $post->excerpt }}</p>
+                            @if ($post->excerpt)
+                                <div class="card-text small text-muted border rounded p-2">
+                                    {{ $post->excerpt }}
+                                </div>
+                            @endif
                         </div>
-                        <div class="card-footer text-end">
-                            <a href="{{ route('blog.show', $post->slug) }}" class="btn btn-primary btn-sm">
-                                <i class="fas fa-arrow-circle-right"></i> Leer ahora
-                            </a>
+                        <div class="card-footer row">
+                            <div class="col-3" style="color: #ccc;">
+                                <span class="p-1"
+                                    style="border-radius:50%;background-color:white;border:0px solid #ccc;width:40px;height:40px;">
+                                    {{ $post->id }}
+                                </span>
+                            </div>
+                            <div class="col-9 text-end">
+                                <a href="{{ route('blog.show', $post->slug) }}" class="btn btn-primary btn-sm">
+                                    <i class="fas fa-arrow-circle-right"></i> Leer ahora
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,27 +1,47 @@
 @extends('layouts.main')
 
 @section('content')
+    <style>
+        .featuredImage {
+            background-image: url('{{ $post->featuredImageUrl() }}');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 600px;
+            margin: 15px auto;
+        }
+
+        .featuredH1 {
+            color: white;
+            text-align: center;
+            font-size: 40px;
+            background-color: black;
+            opacity: 0.9;
+            padding: 1%;
+            margin-top: 260px;
+        }
+    </style>
     <article class="container mt-5">
         <div class="p-1 mt-0 mb-0 border rounded bg-light small">
             <a href="{{ route('blog.index') }}">
                 <i class="fa fa-arrow-left"></i> Artículos
             </a> |
             <a href="javascript:history.back();">
-            Atrás
+                Atrás
             </a>
         </div>
-        <header>
-            <h1 class="my-1">{{ $post->title }}</h1>
-        </header>
-        @if ($post->excerpt)
-            <div class="p-2 mb-2 text-center border rounded" style="background-color:#ffffb9">{{ $post->excerpt }}</div>
-        @endif
+
         <!-- Imagen destacada -->
         @if ($post->featuredImageUrl())
-            <figure class="text-center bg-light border rounded" style="max-height: 400px; overflow: hidden;">
-                <img src="{{ $post->featuredImageUrl() }}" alt="Imagen destacada de {{ $post->title }}"
-                    style="max-height: 400px; width: auto; object-fit: cover;" class="img-fluid">
-            </figure>
+            <header class="featuredImage text-center bg-light border rounded" style="max-height: 400px; overflow: hidden;">
+
+                <h1 class="featuredH1">{{ $post->title }}</h1>
+            </header>
+        @else
+            <h1>{{ $post->title }}</h1>
+        @endif
+        @if ($post->excerpt)
+            <div class="p-2 mb-2 text-center border rounded" style="background-color:#ffffb9">{{ $post->excerpt }}</div>
         @endif
         <section>
             {!! $post->content !!}
