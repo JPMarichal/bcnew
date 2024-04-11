4871a46af06338ce951a8d7cfc0b86becf245236
JPMarichal <jpmarichal@gmail.com>
2024-04-11 01:50:20 -0600

    Se ajusta la fábrica de servicios sociales para usar el contenedor de servicios de Laravel

--- app/Services/SocialMedia/Factories/SocialMediaFactory.php
+++ app/Services/SocialMedia/Factories/SocialMediaFactory.php
@@ -2,16 +2,23 @@
 namespace App\Services\SocialMedia\Factories;
 
 use App\Services\SocialMedia\Contracts\SocialMediaInterface;
-use App\Services\SocialMedia\FacebookService;
+use App\Services\SocialMedia\PinterestService;
 use App\Services\SocialMedia\TelegramService;
-
-// Importar los demás servicios...
+use App\Services\SocialMedia\TwitterService;
+use Illuminate\Support\Facades\App; // Importar la fachada App
 
 class SocialMediaFactory {
     public static function create($type): SocialMediaInterface {
         switch ($type) {
             case 'telegram':
-                return new TelegramService();
+                // Utilizar el contenedor de servicios para resolver la instancia de TelegramService
+                return App::make(TelegramService::class);
+            case 'pinterest':
+                // Utilizar el contenedor de servicios para resolver la instancia de PinterestService
+                return App::make(PinterestService::class);
+            case 'twitter':
+                // Utilizar el contenedor de servicios para resolver la instancia de TwitterService
+                return App::make(TwitterService::class);
             // Agregar casos para los demás servicios...
             default:
                 throw new \Exception("Servicio de red social no soportado.");
