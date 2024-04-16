6b60d8400edb09b2ed698666e799856487acafd3
JPMarichal <jpmarichal@gmail.com>
2024-04-16 14:58:12 -0600

    Ajuste al despliegue de imágenes

--- resources/views/components/ad-placeholder.blade.php
+++ resources/views/components/ad-placeholder.blade.php
@@ -1,3 +1,3 @@
-<div class="ad-placeholder" style="text-align:center; margin: 20px 0;">
+<div class="ad-placeholder" style="text-align:center; margin: 20px 0;background-color:#ddd">
     <img src="https://via.placeholder.com/468x60" alt="Anuncio Simulado">
 </div>
