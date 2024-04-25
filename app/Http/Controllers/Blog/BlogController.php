d09d008cbb4c8d0dc26b5c350407c83e1bd49faa
JPMarichal <jpmarichal@gmail.com>
2024-04-24 20:23:29 -0600

    Se filtran los posts publicados

--- app/Http/Controllers/Blog/BlogController.php
+++ app/Http/Controllers/Blog/BlogController.php
@@ -8,9 +8,31 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    public function indexdd() 
+    {
+        $posts = Post::where('status', 'published')
+            ->where('post_type','post')
+            ->paginate(15);
+        return view('blog.index', compact('posts'));
+    }
+
     public function index()
     {
-        $posts = Post::where('status', 'published')->where('post_type','post')->paginate(15);
+        // Obtener los posts utilizando joins para imitar la consulta SQL proporcionada
+        $posts = Post::select('posts.*')
+                    /* ->join('posts as attachments', function ($join) {
+                         $join->on('posts.id', '=', 'attachments.post_parent')
+                              ->where('attachments.post_type', 'attachment')
+                              ->where('attachments.slug', 'not like', '%b-cdn%');
+                     })
+                     ->join('post_meta', function ($join) {
+                         $join->on('post_meta.meta_value', '=', 'attachments.id')
+                              ->where('post_meta.meta_key', '=', '_thumbnail_id');
+                     }) */
+                     ->where('posts.post_type', 'post')
+                     ->where('posts.status', 'published')
+                     ->paginate(15);
+
         return view('blog.index', compact('posts'));
     }
 
--- app/Models/Blog/Post.php
+++ app/Models/Blog/Post.php
@@ -32,42 +32,33 @@ class Post extends Model
         return $this->belongsTo(User::class, 'author_id');
     }
 
-    /**
-     * Obtiene los comentarios del post.
-     */
     public function comments()
     {
         return $this->morphMany(Comment::class, 'commentable');
     }
 
-    /**
-     * Obtiene los metadatos asociados al post.
-     */
     public function metadata()
     {
         return $this->hasMany(PostMeta::class, 'post_id');
     }
 
-    /**
-     * Obtiene la URL de la imagen destacada del post.
-     */
     public function featuredImageUrl()
     {
-        // Encuentra el PostMeta con la clave _thumbnail_id para este post
         $thumbnailIdMeta = $this->metadata()->where('meta_key', '_thumbnail_id')->first();
-
         if (!$thumbnailIdMeta) {
-            return null; // Retorna null si no se encuentra una imagen destacada
+            return null;
         }
-
-        // Utiliza el meta_value como el ID del post de la imagen destacada
         $thumbnailPostId = $thumbnailIdMeta->meta_value;
-
-        // Encuentra el post de la imagen destacada y devuelve el slug (ruta de la imagen)
         $featuredImagePost = Post::find($thumbnailPostId);
-
         return $featuredImagePost ? $featuredImagePost->slug : '';
     }
 
-    // Agregar cualquier relación adicional aquí
+    /**
+     * Verifica si la imagen destacada contiene 'b-cdn' en su URL.
+     */
+    public function hasCdnImage()
+    {
+        $imageUrl = $this->featuredImageUrl();
+        return $imageUrl ? str_contains($imageUrl, 'b-cdn') : false;
+    }
 }
