0d86b4f70e230238843275f56fcd7db47acfa1fe
JPMarichal <jpmarichal@gmail.com>
2024-04-24 22:13:48 -0600

    Se agregan los autores Anónimo y Desconocido

---
+++ database/seeders/CitaAutoresSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CitaAutoresSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('cita_autores')->insert([
+            ['nombre' => 'Anónimo', 'url_imagen' => null, 'post_id' => null],
+            ['nombre' => 'Desconocido', 'url_imagen' => null, 'post_id' => null]
+        ]);
+    }
+}
