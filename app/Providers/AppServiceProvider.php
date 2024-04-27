d3d5a57bd9b84da71f83ff672134e148b75b7669
JPMarichal <jpmarichal@gmail.com>
2024-04-27 08:42:14 -0600

    Se asegura que el debugbar no se active con las llamadas api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Http\View\Composers\NewsComposer;
+use Illuminate\Support\Facades\Route;
+use Barryvdh\Debugbar\Facade as Debugbar;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       // View::composer(['news.index', 'components.filters'], NewsComposer::class);
+        // View::composer(['news.index', 'components.filters'], NewsComposer::class);
         View::composer(['news.index', 'news.show'], NewsComposer::class);
+
+        // Desactivar Debugbar en rutas API
+        if (Route::current()->getPrefix() === 'api') {
+            Debugbar::disable();
+        }
     }
 }
