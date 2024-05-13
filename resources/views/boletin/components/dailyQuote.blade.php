a98b61b1570a428f18ec55f0d085665714b92afc
JPMarichal <jpmarichal@gmail.com>
2024-05-12 20:53:31 -0600

    Se eliminan blockquotes de la vista de cita diaria.

--- resources/views/boletin/components/dailyQuote.blade.php
+++ resources/views/boletin/components/dailyQuote.blade.php
@@ -1,16 +1,14 @@
 {{-- resources/views/boletin/components/dailyQuote.blade.php --}}
-<div class="quote-section border rounded p-3">
+<div class="quote-section border rounded p-3 my-2">
     @if ($cita)
         <h3>Cita del Día</h3>
-        <blockquote>
-            <h4>{{ $cita->titulo }}</h4>
-            <div class="p-1">
-                {{ $cita->texto }}
-            </div>
-            <div class="mt-2 text-end">
-                {{ $cita->referencia }}
-            </div>
-        </blockquote>
+        <h4>{{ $cita->titulo }}</h4>
+        <div class="p-1">
+            {{ $cita->texto }}
+        </div>
+        <div class="mt-2 text-end">
+            {{ $cita->referencia }}
+        </div>
     @else
         <p>No hay citas disponibles para hoy.</p>
     @endif
