58125fc089146924a5225151fcfdf3bddd57b53c
JPMarichal <jpmarichal@gmail.com>
2024-05-25 22:28:38 -0600

    Modificaciones al formato de impresión.

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -1,15 +1,48 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>{{ $post->title }}</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- FontAwesome -->
+    <link href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" rel="stylesheet">
+
     <style>
         body {
             font-family: 'Helvetica', sans-serif;
         }
+
+        h1 {
+            font-weight: bold;
+        }
+
+        h2 {
+            color: green;
+            font-weight: bold;
+        }
+
+        h3 {
+            color: purple;
+            font-weight: bold;
+        }
+
+        blockquote {
+            margin-left: 40px;
+            margin-right: 40px;
+            background-color: #ddd;
+            padding: 10px;
+            border-left: 3px solid gray;
+        }
     </style>
 </head>
+
 <body>
-    <h1>{{ $post->title }}</h1>
-    <p>{!! $post->content !!}</p>
+    <article class="container mt-5 border rounded p-3">
+        <h1>{{ $post->title }}</h1>
+        <p>{!! $post->content !!}</p>
+    </article>
 </body>
+
 </html>
