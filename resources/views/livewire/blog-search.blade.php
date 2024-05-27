e94c839ae681803c7c1ae12f9e0ac972cfecf3d2
JPMarichal <jpmarichal@gmail.com>
2024-05-27 04:00:29 -0600

    Se estiliza la caja de búsqueda

--- resources/views/livewire/blog-search.blade.php
+++ resources/views/livewire/blog-search.blade.php
@@ -1,4 +1,6 @@
-<div class="mb-4">
-    <input type="text" class="form-control" placeholder="Buscar..." wire:model.debounce.500ms="searchTerm">
-    <button class="btn btn-primary mt-2" wire:click="search">Buscar</button>
+<div class="mb-4 d-flex">
+    <input type="text" class="form-control form-control-sm rounded-pill mr-2" placeholder="Escribe aquí para hacer una búsqueda..." wire:model.debounce.500ms="searchTerm" wire:keydown.enter="search">
+    <button class="btn btn-primary btn-sm d-flex align-items-center" wire:click="search">
+        <i class="fas fa-search"></i> Buscar
+    </button>
 </div>
