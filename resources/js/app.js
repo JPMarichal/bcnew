17b18ee4bf54b6639bfb24362007f4c91f2d502f
JPMarichal <jpmarichal@gmail.com>
2024-06-15 13:17:54 -0600

    Se habilita la creación de componentes en React

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 import '../sass/styles.scss';
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import Alpine from 'alpinejs';
 
